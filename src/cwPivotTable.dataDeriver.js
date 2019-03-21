9bdac3767a4a38779d47c5793440df05cbb2df4c
nevakee716 <nevakee716@gmail.com>
2019-03-21 12:09:10 +0100

    correct syntax

--- src/cwPivotTable.dataDeriver.js
+++ src/cwPivotTable.dataDeriver.js
@@ -62,7 +62,7 @@
     if(this.config.currentDateFilter !== undefined) {
       let d = new Date()
       for(let i in this.config.currentDateFilter) {
-        this.config.inclusions[this.config.currentDateFilter[i]] = d.getFullYear() + "/" + d.getMonth();
+        this.config.inclusions[this.config.currentDateFilter[i]] = [d.getFullYear() + "/" + d.getMonth()];
       }
     }
     return this.config.inclusions;
